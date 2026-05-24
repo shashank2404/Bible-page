d692538b4cb56d67621d0e25cfba385865ece959
shashank2404 <shashanklal43@gmail.com>
2026-05-24 18:29:02 +0530

    needed 3

--- src/pages/ArticleDetailWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-import ArticleDetail from "./Articledetail";
-
-const ArticleDetailWrapper = () => {
-  const { id } = useParams();
-  return <ArticleDetail articleId={id} />;
-};
-
-export default ArticleDetailWrapper;
