f02a9640c091c012642ce80e1911b4862701b35a
shashank2404 <shashanklal43@gmail.com>
2026-05-27 21:45:18 +0530

    sigin 3

--- src/utils/auths.js
+++ src/utils/auths.js
@@ -1,6 +1,6 @@
 export const API_BASE = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
   ? "http://localhost:5000"
-  : "https://thebibleglory.com";
+  : window.location.origin;
 
 export const saveToken = (token, name) => {
   localStorage.setItem("token", token);
