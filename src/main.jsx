af48d2de42345910fc7ef565ff92e10ce3857b23
shashank2404 <shashanklal43@gmail.com>
2026-05-27 13:47:05 +0530

    signin

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || "151930659692-45vnrt80f7r9nlr3bun4qe0p9pae1jim.apps.googleusercontent.com"}>
       <App />
     </GoogleOAuthProvider>
   </StrictMode>
